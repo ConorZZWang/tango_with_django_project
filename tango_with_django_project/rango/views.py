c52c5b743af53eafd3ff994d82f368f81f873672
“Conor <2722929w@student.gla.ac.uk>
2024-02-09 01:41:39 +0000

    C10 finished

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,16 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
+from rango.models import Category, Page
 from django.shortcuts import redirect
-from rango.forms import PageForm
-from rango.forms import UserForm, UserProfileForm
-from django.http import HttpResponse
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
-from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
 
 # Create your views here.
 
@@ -19,24 +15,26 @@ def index(request):
 # Order the categories by the number of likes in descending order.
 # Retrieve the top 5 only -- or all if less than 5.
 # Place the list in our context_dict dictionary (with our boldmessage!) # that will be passed to the template engine.
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('views')[:5]
+    category_list = Category.objects.order_by('-likes')[:5] 
+    page_list = Page.objects.order_by('-views')[:5]
+    
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    # Render the response and send it back!
-    request.session.set_test_cookie()
-    return render(request, 'rango/index.html', context=context_dict)
+    
+    visitor_cookie_handler(request)
+    return render(request, 'rango/index.html', context=context_dict) 
+    
+
 
 
 def about(request):
-    print(request.method)
-    if request.session.test_cookie_worked(): 
-        print("TEST COOKIE WORKED!") 
-        request.session.delete_test_cookie()
-    # prints out the user name, if no one is logged in it prints `AnonymousUser` print(request.user)
-    return render(request, 'rango/about.html', {})
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -202,9 +200,33 @@ def user_login(request):
 def restricted(request):
     return render(request, 'rango/restricted.html')
 # Use the login_required() decorator to ensure only those logged in can # access the view.
+
 @login_required
 def user_logout(request):
     # Since we know the user is logged in, we can now just log them out.
     logout(request)
     # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None): 
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val 
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1')) 
+    last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count 
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie 
+        request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+    request.session['visits'] = visits
